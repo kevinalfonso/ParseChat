7413d0c2f22c8c5283e9574502865648a1de5e2b
Kevin Alfonso <kevin@kevins-mbp.fios-router.home>
2017-02-23 23:52:32 -0500

    commit

--- ParseChat/LoginViewcontroller.swift
+++ ParseChat/LoginViewcontroller.swift
@@ -13,8 +13,52 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signupButton: UIButton!
-    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func signUp(_ sender: Any) {
+        let user = PFUser()
+        user.email = emailTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground {
+            (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                self.showAlert(errorMsg: error.localizedDescription)
+            } else {
+                // Hooray! Let them use the app now.
+                //self.loginSegue()
+            }
+            
+        }
+    }
+
+    @IBAction func logIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!) { (succedded: PFUser?, error: Error?) -> Void in
+            if let error = error {
+                self.showAlert(errorMsg: error.localizedDescription)
+            } else {
+                // Hooray! Let them use the app now.
+                self.loginSegue()
+            }
+        }
+    }
+    
+    func showAlert(errorMsg: String) {
+        let alertController = UIAlertController(title: "Error", message: errorMsg, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+        }
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true) {
+            
+        }
+    }
+    
+    func loginSegue(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NavigationController")
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,24 +69,6 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func signUp() {
-        let user = PFUser()
-        user.email = emailTextField.text
-        user.password = passwordTextField.text
-        
-        user.signUpInBackground(block: success: Bool, error: NSError)
-        
-//        user.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-//            if let error = error {
-//                print(error.localizedDescription)
-//            } else {
-//                print("User Registered successfully")
-//                // manually segue to logged in view
-//            }
-//        }
-    }
-    
     
 
 
